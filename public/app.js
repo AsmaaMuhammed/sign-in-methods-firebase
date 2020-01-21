8aec8ae44f2df634818be73206ceeec2791f5531
asmaa <asmmohhasanien@gmail.com>
2020-01-21 02:34:41 +0200

    first commit

---
+++ public/app.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBoXPU76JvC3ueee4rRACfR96cBRKuNdAA",
+    authDomain: "personal-trainer-pt.firebaseapp.com",
+    databaseURL: "https://personal-trainer-pt.firebaseio.com",
+    projectId: "personal-trainer-pt",
+    storageBucket: "personal-trainer-pt.appspot.com",
+    messagingSenderId: "553151763334",
+    appId: "1:553151763334:web:6227b8a9600c361db7d207",
+    measurementId: "G-53F1TCTB5Q"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// var db = firebase.firestore();
+// // Get a reference to the storage service, which is used to create references in your storage bucket
+// var storage = firebase.storage();
+// var storageRef = storage.ref();
+ 
